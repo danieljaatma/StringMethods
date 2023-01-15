e42f4f3fb0fd22b52c24cc5220d0fe38c1f4fda9
danieljaatma <daniel.jaatma@gmail.com>
2023-01-15 18:01:50 +0200

    Stringidega seotud tegevused.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String playerName = "Lionel Messi";
+        String playerName2 = "Lionel Messi";
+        boolean areEqual = playerName.equals(playerName2); // Võrdleb kaht muutujat
+        int playerNameLength = playerName.length(); // toob 12 tähte
+        char charAt = playerName.charAt(4); // toob viienda tähe
+        int indeks = playerName.indexOf("e"); // otsib õige indeksi
+        String subString = playerName.substring(2, 5); // Võtab nende vahel oleva teksti.
+
+        System.out.println(areEqual);
+        System.out.println(playerNameLength);
+        System.out.println(charAt);
+        System.out.println(indeks);
+        System.out.println(subString);
+
     }
 }
+
